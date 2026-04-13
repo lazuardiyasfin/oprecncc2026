8163e04872866c7e97430f6a73d8f36806b927e0
lazuardiyasfin <70686532+lazuardiyasfin@users.noreply.github.com>
2026-04-14 00:35:47 +0700

    feat: add sqlite db integration
    The db integration is needed to keep track the mappings between
    the original file name and the UID.

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"database/sql"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -8,11 +12,56 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
+	_ "modernc.org/sqlite"
 )
 
+var db *sql.DB
+
+func initDatabase(path string) error {
+	var err error
+	db, err = sql.Open("sqlite", path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.ExecContext(
+		context.Background(),
+		`CREATE TABLE IF NOT EXISTS files (
+			id TEXT PRIMARY KEY,
+			name TEXT,
+			extension TEXT,
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+			expires_at DATETIME
+		)`,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
 func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{"status": "healthy"})
+}
+
+func generateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func generateRandomString(s int) (string, error) {
+	b, err := generateRandomBytes(s)
+	return base64.RawURLEncoding.EncodeToString(b), err
 }
 
 func fileUploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -37,8 +86,27 @@ func fileUploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer uploadedFile.Close()
 
-	filename := header.Filename
-	targetFile, err := os.Create("uploads/" + filename)
+	id, err := generateRandomString(12)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	filename := filepath.Base(header.Filename)
+	ext := filepath.Ext(filename)
+	name := filename[:len(filename) - len(ext)]
+
+	now := time.Now()
+	expiredAt := now.Add(24 * time.Hour)
+
+	_, err = db.ExecContext(
+		context.Background(),
+		`INSERT INTO files (id, name, extension, expires_at) VALUES (?, ?, ?, ?)`, id, name, ext, expiredAt,
+	)
+
+	path := filepath.Join("uploads", id + ext)
+	
+	targetFile, err := os.Create(path)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -51,24 +119,46 @@ func fileUploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("localhost:8080/d/" + filename))
+	w.Header().Set("Content-Type", "plain/text")
+	message := fmt.Sprintf("File successfully uploaded!\nDownload link: http://localhost:8080/d/%s\n", id)
+	w.Write([]byte(message))
 }
 
 func fileDownloadHandler(w http.ResponseWriter, r *http.Request) {
-	filename := r.URL.Path[len("/d/"):]
-	if filename == "" {
-		http.Error(w, "Missing filename", http.StatusBadRequest)
+	id := r.URL.Path[len("/d/"):]
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
+	var name, ext string
+
+	row := db.QueryRowContext(
+		context.Background(),
+		`SELECT name, extension FROM files WHERE id=?`, id,
+	)
+
+	err := row.Scan(&name, &ext)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	filename := name + ext
 	contentDisposition := fmt.Sprintf("attachment; filename=\"%s\"", filename)
 	w.Header().Set("Content-Disposition", contentDisposition)
 
-	filepath := filepath.Join("uploads", filename)
+	filepath := filepath.Join("uploads", id + ext)
 	http.ServeFile(w, r, filepath)
 }
 
 func main() {
+	initDatabase("app.db")
+
 	http.HandleFunc("/health", healthHandler)
 	http.HandleFunc("/upload", fileUploadHandler)
 	http.HandleFunc("/d/", fileDownloadHandler)
